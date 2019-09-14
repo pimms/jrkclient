4656205a808557e41cda872a2c2fb694fcfd3aa6
Joakim Stien <joakim.stien@gmail.com>
2019-09-14 17:53:55 +0200

    Design changes to PlayViewController

--- newjrk/Views/GradientBlurView.swift
+++ newjrk/Views/GradientBlurView.swift
@@ -3,28 +3,14 @@ import UIKit
 @IBDesignable
 class GradientBlurView: UIView {
 
-    // MARK: - IBDesignable properties
-
-    @IBInspectable
-    var blurHeight: CGFloat = 100 {
-        didSet { updateGradientEndpoint() }
-    }
-
     // MARK: - Private properties
 
-    private let blurEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+    private let blurEffect = UIBlurEffect(style: .systemThickMaterial)
 
     private lazy var effectView: UIVisualEffectView = {
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.translatesAutoresizingMaskIntoConstraints = false
         return blurView
-    }()
-
-    private lazy var vibrancyView: UIVisualEffectView = {
-        let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
-        let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
-        vibrancyView.translatesAutoresizingMaskIntoConstraints = false
-        return vibrancyView
     }()
 
     private lazy var gradientLayer: CAGradientLayer = {
@@ -55,14 +41,16 @@ class GradientBlurView: UIView {
 
         backgroundColor = .clear
         super.addSubview(effectView)
-        effectView.contentView.addSubview(vibrancyView)
-
         effectView.fillInSuperview()
-        vibrancyView.fillInSuperview()
     }
 
     private func addGradientLayer() {
-        gradientLayer.colors = [ UIColor.clear.cgColor, UIColor.white.cgColor ]
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.white.cgColor,
+            UIColor.clear.cgColor
+        ]
+        gradientLayer.locations = [NSNumber(value: 0), NSNumber(value: 0.5), NSNumber(value: 1.0)]
         effectView.layer.mask = gradientLayer
     }
 
@@ -71,53 +59,11 @@ class GradientBlurView: UIView {
         updateGradientEndpoint()
     }
 
-    // MARK: - Private func
+    // MARK: - Private methods
 
     private func updateGradientEndpoint() {
-        let ratio = blurHeight / bounds.height
         gradientLayer.frame = bounds
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: ratio)
-    }
-}
-
-// MARK: - Content View overrides
-// Simply proxy all subview-related calls to
-
-extension GradientBlurView {
-    override func addSubview(_ view: UIView) {
-        vibrancyView.contentView.addSubview(view)
-    }
-
-    override func willRemoveSubview(_ subview: UIView) {
-        vibrancyView.contentView.willRemoveSubview(subview)
-    }
-
-    override func insertSubview(_ view: UIView, at index: Int) {
-        vibrancyView.contentView.insertSubview(view, at: index)
-    }
-
-    override func sendSubviewToBack(_ view: UIView) {
-        vibrancyView.contentView.sendSubviewToBack(view)
-    }
-
-    override func bringSubviewToFront(_ view: UIView) {
-        vibrancyView.contentView.bringSubviewToFront(view)
-    }
-
-    override func exchangeSubview(at index1: Int, withSubviewAt index2: Int) {
-        vibrancyView.contentView.exchangeSubview(at: index1, withSubviewAt: index2)
-    }
-
-    override func insertSubview(_ view: UIView, aboveSubview siblingSubview: UIView) {
-        vibrancyView.contentView.insertSubview(view, aboveSubview: siblingSubview)
-    }
-
-    override func insertSubview(_ view: UIView, belowSubview siblingSubview: UIView) {
-        vibrancyView.contentView.insertSubview(view, belowSubview: siblingSubview)
-    }
-
-    override func didAddSubview(_ subview: UIView) {
-        vibrancyView.contentView.didAddSubview(subview)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
     }
 }
