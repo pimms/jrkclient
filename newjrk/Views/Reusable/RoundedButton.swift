2f5ae67886b3e0b9d4fc10793cd76328f9411b9f
Joakim Stien <joakim.stien@gmail.com>
2019-09-15 14:45:18 +0200

    Added IBDesignable RoundedButton

---
+++ newjrk/Views/Reusable/RoundedButton.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        set { layer.cornerRadius = newValue }
+        get { return layer.cornerRadius }
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    private func setup() {
+        clipsToBounds = true
+        setTitle("Rounded Button", for: .normal)
+    }
+}
